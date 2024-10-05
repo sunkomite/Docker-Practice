2d69a8840303c252ce5600552eed413184b3fd10
Habeeb <sunkomite@yahoo.co.uk>
2024-10-05 21:20:34 +0100

    i made my changes to my profile

--- docker.tf
+++ docker.tf
@@ -1,7 +1,7 @@
 # configured aws provider with proper credentials
 provider "aws" {
   region = "us-east-1"
-  profile= "Abdulhakeem"
+  profile= "Habeeb"
 }
 
 # Create a remote backend for your terraform 
@@ -11,7 +11,7 @@ terraform {
     dynamodb_table = "app-state"
     key    = "LockID"
     region = "us-east-1"
-    profile = "Abdulhakeem"
+    profile = "Habeeb"
   }
 }
 
@@ -116,7 +116,7 @@ resource "aws_instance" "ec2_instance1" {
   instance_type          = "t2.micro"
   subnet_id              = aws_default_subnet.default_az1.id
   vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
-  key_name               = "kim"
+  key_name               = "baggio"
   user_data            = "${file("docker-install.sh")}"
 
   tags = {
